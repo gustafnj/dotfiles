7291ace6a5c2dea006d624f05de8b020058f06ec
gustafnj <njgustafson@gmail.com>
2015-11-25 16:12:50 -0600

    punt for now on clone

--- setup.sh
+++ setup.sh
@@ -1,24 +1,19 @@
+#!/bin/zsh -e
+
 # install homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew install gcc git postgresql tmux
 
-# pythonz
-curl -kL https://raw.github.com/saghul/pythonz/master/pythonz-install | bash
-pythonz install 2.7.10
-sudo easy_install pip
-sudo pip install virtualenv
-sudo pip install virtualenvwrapper
-
 VIM_BUNDLE_PATH=${HOME}/.vim/bundle
-GITHUB_PATH=https://github.com
+VIM_AUTOLOAD_PATH=${HOME}/.vim/autoload
 
 # install vim pathogen
-mkdir -p "${HOME}/.vim/autoload" "${HOME}/.vim/bundle"
-curl -LSso "${HOME}/.vim/autoload/pathogen.vim" https://tpo.pe/pathogen.vim
+mkdir -p ${VIM_AUTOLOAD_PATH} ${VIM_BUNDLE_PATH}
+curl -LSso "${VIM_AUTOLOAD_PATH}/pathogen.vim" https://tpo.pe/pathogen.vim
 
 # get github repos
-VIM_PKGS='
+VIM_PKGS=(
     bling/vim-airline 
     hdima/python-syntax 
     klen/python-mode 
@@ -28,18 +23,35 @@ VIM_PKGS='
     vim-scripts/Align 
     voithos/vim-python-matchit 
     altercation/vim-colors-solarized 
-'
+)
 
+GITHUB_URL=https://github.com
 for PKG in $VIM_PKGS; do 
     PKG_NAME=${PKG##*/}
-    git clone "${GITHUB_PATH}/${PKG}" "${VIM_BUNDLE_PATH}/${PKG_NAME}"
+    git clone "${GITHUB_URL}/${PKG}" "${VIM_BUNDLE_PATH}/${PKG_NAME}"
 done
 
 # get oh-my-zsh
-OH_MY_ZSH='robbyrussell/oh-my-zsh'
-git clone "${GITHUB_PATH}/${OH_MY_ZSH}" "${HOME}/.oh-my-zsh"
+OH_MY_ZSH_REPO='robbyrussell/oh-my-zsh.git'
+git clone "${GITHUB_URL}/${OH_MY_ZSH_REPO}" "${HOME}/.oh-my-zsh"
 
 DOTFILES_PATH=/Users/ng/src/dotfiles
-ln -s "${DOTFILES_PATH}/.vimrc" "${HOME}/.vimrc"
-ln -s "${DOTFILES_PATH}/.zshrc" "${HOME}/.zshrc"
-ln -s "${DOTFILES_PATH}/.tmux.conf" "${HOME}/.tmux.conf"
+DOTFILES=(
+    .vimrc 
+    .tmux.conf 
+    .zshrc
+)
+
+for DOTFILE in $DOTFILES; do
+    TARGET_PATH=${HOME}/${DOTFILE}
+    ln -s "${DOTFILES_PATH}/${DOTFILE}" ${TARGET_PATH}
+done
+
+# pythonz
+curl -kL https://raw.github.com/saghul/pythonz/master/pythonz-install | bash
+ZSHRC_PATH=${TARGET_PATH}
+source ${ZSHRC_PATH}
+pythonz install 2.7.6
+sudo easy_install pip
+sudo pip install virtualenv
+sudo pip install virtualenvwrapper

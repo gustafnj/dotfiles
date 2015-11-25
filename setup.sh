682e5c5ff46e3b2ea9d6725f99c108a1c60bb728
gustafnj <njgustafson@gmail.com>
2015-11-25 13:13:09 -0600

    add wip script for setting up env

---
+++ setup.sh
@@ -0,0 +1,37 @@
+# install homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+VIM_BUNDLE_PATH=${HOME}/.vim/bundle
+GITHUB_PATH=https://github.com
+
+# install vim pathogen
+mkdir -p "${HOME}/.vim/autoload" "${HOME}/.vim/bundle"
+curl -LSso "${HOME}/.vim/autoload/pathogen.vim" https://tpo.pe/pathogen.vim
+
+# get github repos
+VIM_PKGS='
+    bling/vim-airline 
+    hdima/python-syntax 
+    klen/python-mode 
+    scrooloose/syntastic 
+    scrooloose/nerdtree 
+    tpope/vim-surround 
+    vim-scripts/Align 
+    voithos/vim-python-matchit 
+    altercation/vim-colors-solarized 
+'
+
+for PKG in $VIM_PKGS; do 
+    PKG_NAME=${PKG##*/}
+    echo $PKG_NAME
+    git clone "${GITHUB_PATH}/${PKG}" "${VIM_BUNDLE_PATH}/${PKG_NAME}"
+done
+
+# get oh-my-zsh
+OH_MY_ZSH='robbyrussell/oh-my-zsh'
+git clone "${GITHUB_PATH}/${OH_MY_ZSH}" "${HOME}/.oh-my-zsh"
+
+DOTFILES_PATH=/Users/ng/src/dotfiles
+ln -s "${DOTFILES_PATH}/.vimrc" "${HOME}/.vimrc"
+ln -s "${DOTFILES_PATH}/.zshrc" "${HOME}/.zshrc"
+ln -s "${DOTFILES_PATH}/.tmux.conf" "${HOME}/.tmux.conf"
